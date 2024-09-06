c01e475337a5737e3f380036a51b8894691aa35e
Ivan Maximiliano Korner <ivanmaximilianokorner@MacBook-Pro-de-Ivan.local>
2024-09-06 17:53:00 -0300

    Modificando varias cosas

--- vistas/home.php
+++ vistas/home.php
@@ -1,7 +1,7 @@
 <div class="container home-div">
   
-  <div class="px-4 pt-5 my-5 text-center" style="height:90vh">
-    <h1 class="display-4 fw-bold text-body-emphasis">Bienvenidos</h1>
+  <div class="px-4 pt-5 my-5 text-center">
+    <h1 class="display-4 fw-bold text-body-emphasis">Bienvenido @Usuario</h1>
     <div class="col-lg-6 mx-auto">
       <p class="lead mb-4">Quickly design and customize responsive mobile-first sites with Bootstrap, the world’s most popular front-end open source toolkit, featuring Sass variables and mixins, responsive grid system, extensive prebuilt components, and powerful JavaScript plugins.</p>
       <div class="d-grid gap-2 d-sm-flex justify-content-sm-center mb-5">
